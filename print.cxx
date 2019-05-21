7968a46fb8b33688de4f4a33b556029ec3284057
anandhkrishna <45140249+anandhkrishna@users.noreply.github.com>
2019-05-21 22:58:29 +0530

    Add files via upload

---
+++ print.cxx
@@ -0,0 +1,13 @@
+#include  <iostream>    
+//pre-processer _include_iostream header
+using namespace std;    // standard namespace exspecialy for c++
+int main() // main function with integer(number) return type.
+
+{ 
+     cout <<"*************************" << endl;
+   //  output function like printing on display.
+     cout <<"\tWELCOME TO C++\n" ;  //endl is alternative newline move key for \n..
+     cout << "*************************";
+     return 0; // returning with integer zero
+	
+}
