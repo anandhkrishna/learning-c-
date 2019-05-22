6c88cf18c2bd5320ee74b63fe2fecba64cef4250
anandhkrishna <45140249+anandhkrishna@users.noreply.github.com>
2019-05-22 08:05:24 +0530

    Add files via upload

---
+++ add.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,sum;    //variable decleration
+	cout <<"*******************************"<<endl;
+	cout << "enter your first number"<<endl;
+	cin >> a;
+	cout <<"enter your second number "<<endl;
+	cin >> b;
+    sum = a+ b; // addition operation
+    cout << "sum is  "<<sum<<endl;
+    cout <<"*******************************";
+    return 0;
+}
